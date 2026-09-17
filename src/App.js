142aa1508ae833a2908cce7147f2ffd71ad7af25
Aleix <anfifus@gmail.com>
2026-09-17 03:41:13 +0200

    Canvi direcció url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         element:<Formulari />
       },
       {
-        path:"/Usuari/:id",
+        path:"/:id",
         element:<dadesUser />
       }
     ]
